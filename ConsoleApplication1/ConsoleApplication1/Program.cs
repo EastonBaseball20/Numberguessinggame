f05b658de4cca021cf975ac13e5bc9fc9991a660
EastonBaseball20 <raisnerw@gmail.com>
2014-03-05 10:01:29 -0500

    hdfdf

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+
+            int guess = 50;
+            Console.WriteLine("Is Your Guess {0}? h, l, e," guess);
+            input= Console.ReadLine();
+
+            if (input == "h")
+        }
+        Console.WriteLine("is your number 75
+    }
+}
+
+
